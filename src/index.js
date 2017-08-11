52e073c9fe8bb19bfa6a4ffaa1217effa6a9aa70
Sam Humphreys <s-humphreys@hotmail.com>
2017-08-11 11:37:13 +1000

    todos rendering

--- src/index.js
+++ src/index.js
@@ -1,19 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ShowTodos from './components/show-todos';
+import http from 'http';
 
-class App extends React.Component {
+class Todos extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      todos: null
+    }
+  };
+
+  componentWillMount () {
+    http.get('http://localhost:8000/api/todos', (res) => {
+      res.on('error', (err) => console.error(err));
+      res.on('data', (todos) => {
+        this.setState({todos: JSON.parse(todos.toString())});
+      });
+    });
+  };
+
+  listTodos () {
+    return (
+      this.state.todos.map((todo) => {
+        console.log(todo);
+        return (
+          <div className='todo'
+                key={todo.id}>
+            {todo.title}
+          </div>
+        )
+      })
+    );
+  }
+
 
   render () {
+    if (!this.state.todos) {
+      return (
+        <div className='show-todos'>
+          loading spinner...
+        </div>
+      )
+    };
     return (
-      <div className='App'>
-        <ShowTodos />
+      <div className='todos-list'>
+        {this.listTodos()}
       </div>
     )
   }
 };
 
 ReactDOM.render(
-  <App/>,
+  <Todos />,
   document.getElementById('App')
 );

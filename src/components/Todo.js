aac3803302e3ac9705a34d5a4fd41d00733934b9
Sam Humphreys <s-humphreys@hotmail.com>
2017-08-14 13:56:28 +1000

    added temp style sheets

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import TodoItem from './TodoItem';
+import '../styles/Todo.css'
 
 export default function Todo (props) {
-  if (props.selectedTodo === props.data.id) {
-    console.log('selected props', props);
+
+  const showTodoItems = () => {
+    if (props.selectedTodo === props.data.id) {
+      return (
+        <div className='todo-items'>
+          <button>add todo item</button>
+          {props.data.todoItems.map((todoItem) => {
+            return (
+              <TodoItem className='todo-item'
+                          key={todoItem.id}
+                          data={todoItem} />
+            )
+          })}
+        </div>
+      );
+    };
   };
+
   return (
     <div className='todo'
           onClick={() => props.handleTodoClick(props.data.id)}>
       {props.data.title}
+      {showTodoItems()}
     </div>
   )
 };
---
+++ src/components/TodoItem.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/TodoItem.css'
+
+export default function TodoItem (props) {
+  return (
+    <div className='todo-item'>
+      hello
+    </div>
+  )
+};

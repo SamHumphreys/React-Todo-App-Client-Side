8ae6c007d8228061655e6c2656763c4bf26a5995
Sam Humphreys <s-humphreys@hotmail.com>
2017-08-24 14:53:41 +1000

    selected todo working

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,27 +1,31 @@
 import React from 'react';
-import TodoItems from './TodoItems';
 import '../styles/Todo.css'
 
+const Todo = (props) => {
+  console.log(props);
 
-export default class Todo extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      todo: this.props.todo,
-      completedPercent: 0
-    }
-  };
-
-  render () {
+  const showTodo = () => {
     return (
-      <div className='todo'
-            onClick={() => this.props.handleTodoClick(this.state.todo.id)}>
-        {this.state.todo.title}
-        <TodoItems todoItems={this.state.todo.todoItems}
-                    todoId={this.state.todo.id}
-                    selectedTodo={this.props.selectedTodo} />
+      <div onClick={() => props.handleTodoClick(props.data.id)}>
+        {props.data.title}
       </div>
     );
   };
+
+  const showItems = () => {
+    return <div>Items</div>;
+  };
+
+  if (props.selectedTodo === props.data.id) {
+    return (
+      <div className='todo'>
+        {showTodo()}
+        {showItems()}
+      </div>
+    )
+  } else {
+    return showTodo()
+  }
 };
+
+export default Todo;
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,21 +1,42 @@
 import React from 'react';
+import Todo from './Todo';
 
-const Todos = (props) => {
-  console.log(props);
-
-  const addTodo = () => {
-    const newTask = prompt('What is the task to complete?');
-    props.addTodo(newTask);
+export default class Todos extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedTodo: null
+    };
   };
 
-  return (
-    <div className='todos-list'>
-      <button onClick={() => addTodo()}>Add a task...</button>
-      {props.todos.map((todo) => {
-        return <div key={todo.id}>{todo.id}</div>
-      })}
-    </div>
-  )
-};
+  addTodo () {
+    const newTask = prompt('What is the task to complete?');
+    this.props.addTodo(newTask);
+  };
 
-export default Todos;
+  todoSelected (selectedTodo) {
+    if (this.state.selectedTodo !== selectedTodo) {
+      this.setState({selectedTodo});
+    };
+  };
+
+  showTodos () {
+    return (
+      this.props.todos.map((todo) => {
+        return <Todo key={todo.id}
+                      data={todo}
+                      handleTodoClick={(id) => this.todoSelected(id)}
+                      selectedTodo={this.state.selectedTodo} />
+      })
+    );
+  };
+
+  render () {
+    return (
+      <div className='todos-list'>
+        <button onClick={() => this.addTodo()}>Add a task...</button>
+        {this.showTodos()}
+      </div>
+    );
+  };
+};
